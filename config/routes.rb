01a177583616feff9a0b524355b87bfacaa710ba
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-08 16:25:23 +0200

    change root path to home and add for it test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  #get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/contact'
-  root 'application#hello'
+  get 'hello' =>'application#hello'
   get 'test'=>'application#test'
   get 'test2' => 'application#test2'
   get 'static_pages/about'
+  root 'static_pages#home'
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
