3188dde7da432c93ba61a81de7fe3436c479f543
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-11 15:44:37 +0200

    change to the path to be non static, change in test to get paths not static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
   get 'hello' =>'application#hello'
   get 'test'=>'application#test'
   get 'test2' => 'application#test2'
-  get 'static_pages/about'
   root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  #pod adresem /help będzie można wyświetlić views help z konrollera static_pages akcję help
   #path_names {new: ''}
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,19 +13,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
  # end
 
  test "should get help" do
-   get static_pages_help_url
+   #get static_pages_help_url pobiera tylko statyczne url
+   get help_path
    assert_response :success
    assert_select "title", "Help | #{@base_title}"
  end
 
  test "should get about" do
-   get static_pages_about_url
+   get about_path
    assert_response :success
    assert_select "title", "About | #{@base_title}"
  end
 
  test "should get contact" do
-   get static_pages_contact_url
+   get contact_path
    assert_response :success
    assert_select "title", "Kontak | #{@base_title}"
  end
@@ -37,7 +38,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # end
 
    test "should get home" do
-     get static_pages_home_url
+     get root_path
      assert_response :success
      assert_select "title", "Ruby on Rails Tutorial Sample App"
    end
