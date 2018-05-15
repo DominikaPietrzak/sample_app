44f8bcd3480ecc2c6f8d534cd1e247089de653db
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-15 16:13:18 +0200

    add test for password presence and min length, add password for users, add secure_password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -52,6 +52,16 @@ test "email addresses should be saved as lower-case" do
   @user.email = mixed_case_email
   @user.save
   assert_equal mixed_case_email.downcase, @user.reload.email
+end
+
+test "password should be present (nonblank)" do
+  @user.password = "   " * 6
+  assert_not @user.valid?
+end
+
+test "password should have minimum length" do
+  @user.password = "a" * 5
+  assert_not @user.valid?
 end
 
 end
