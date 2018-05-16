e4dc50a05b0e2e54efca8ce76668bae45cceef17
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-16 17:14:53 +0200

    add views for showing errors in views and add, permited parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
 
     else
@@ -17,8 +17,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-     :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
