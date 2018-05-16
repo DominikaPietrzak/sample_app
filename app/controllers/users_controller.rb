e4221c5e1c25d62bdeace6dcaf24987aeda13f21
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-16 16:44:21 +0200

    add form in new user and add create controller and permit options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+     :password_confirmation)
+  end
+
 end
