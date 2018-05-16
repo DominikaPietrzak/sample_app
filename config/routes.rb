ef34131bac0e05d2d9da8d0efb50106faa1d25fa
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-16 10:51:06 +0200

    add users routes, add users show controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  resources :users
   #pod adresem /help będzie można wyświetlić views help z konrollera static_pages akcję help
   #path_names {new: ''}
 end
