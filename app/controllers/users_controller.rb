a5333aa0763da5e24b3347832b98931b739fa868
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-17 13:23:47 +0200

    add flash messages to show if someone log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample page"
     else
       render 'new'
+      flash[:danger] = "Failure"
     end
   end
 
