2a09b9cd994cf805bd8c125ec96d25769489e0c6
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-17 12:12:05 +0200

    change form_for and add post routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  post 'signup', to: 'users#create'
   resources :users
   #pod adresem /help będzie można wyświetlić views help z konrollera static_pages akcję help
   #path_names {new: ''}
