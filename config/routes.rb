d92179351dbae9c0e53480bd9da2f72cf0258345
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-17 15:14:38 +0200

    add controller, views, test,  for session,

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get 'static_pages/home'
   get 'hello' =>'application#hello'
   get 'test'=>'application#test'
