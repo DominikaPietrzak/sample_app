7367fdfadf328acba3b48a1a0e14bd21199301a9
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-22 15:31:06 +0200

    add index page for users,  custom css for styling users, add test for not show users that was not logged in, add link to all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit, :update]
 
 
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
@@ -54,7 +57,5 @@ class UsersController < ApplicationController
     @user =  User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-
-
 
 end
