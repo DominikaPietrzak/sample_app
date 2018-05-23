36f48c7deec3cd9566c94b2360624b1f782c605c
DominikaPieRep <dompie6@st.amu.edu.pl>
2018-05-23 12:54:50 +0200

    add possibylity to delete user but only for admin, add tests to delete users, and showing links and pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,20 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
-
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
   end
-  
+
   def new
     @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = " User deleted"
+    redirect_to users_url
   end
 
   def show
@@ -59,4 +65,7 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
